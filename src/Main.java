153c5342b2e1a58e5ac02d5f9800e096b7288d0c
Hiago 7k <hiago7k@gmail.com>
2026-07-06 17:03:18 -0300

    finalizando outro teste

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 class Main {
@@ -6,7 +7,7 @@ class Main {
         /* Crie um programa que solicite ao usuário digitar um número.
         Se o número for positivo, exiba "Número positivo", caso contrário,
         exiba "Número negativo".
-        */
+
 
         Scanner entrada = new Scanner(System.in);
         System.out.println("Digite um número: ");
@@ -17,5 +18,36 @@ class Main {
         }else {
             System.out.println("Número negativo " + numero);
         }
+
+         */
+
+        // 2
+        /*
+         Peça ao usuário para inserir dois números inteiros.
+         Compare os números e imprima uma mensagem indicando
+         se são iguais, diferentes, o primeiro é maior ou o segundo é maior.
+         */
+
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite um número inteiro exp: 1,2,3..: ");
+        int numUm = leitor.nextInt();
+        System.out.println("Digite o segundo número inteiro: ");
+        int numDois = leitor.nextInt();
+
+
+        if (numUm >= numDois){
+            System.out.println("O número "+  numUm + " " + " é maior ou igual ao segundo numero que voce digitou : " + numDois );
+        } else if (numUm == numDois) {
+
+        } else {
+            System.out.println("O número "+  numDois + " " + " é maior que o primeiro numero que voce digitou  "  + numUm);
+        }
+        
+        if(numUm == numDois){
+            System.out.println("Os números são iguais " + numUm + " | " + numDois);
+        }else {
+            System.out.println("os números são diferentes " + numUm + " | " + numDois);
+        }
+
     }
 }
